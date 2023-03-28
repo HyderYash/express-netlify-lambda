cd32fa6280646f843085e56c50ce0bc8d81657f9
HyderYash <yashsharma.karate@gmail.com>
2023-03-28 18:21:56 +0530

    10

--- src/callListAllSkus.js
+++ src/callListAllSkus.js
@@ -1,8 +1,6 @@
-const { schedule } = require("@netlify/functions");
-
 const fetch = require("node-fetch").default;
 
-const handler = async () => {
+export const handler = async () => {
   const baseUrl =
     "https://incredible-cocada-df49b8.netlify.app/.netlify/functions/app/listallskus";
   const response = await fetch(baseUrl, {
@@ -15,4 +13,3 @@ const handler = async () => {
     body: JSON.stringify(response),
   };
 };
-exports.handler = schedule("*/1 * * * *", handler);
